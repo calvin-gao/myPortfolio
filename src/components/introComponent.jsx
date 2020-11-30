85c3e1415a5809a5aaf5908e2103d69671a2d98c
Calvin Gao <c_gao3@yahoo.com>
2020-11-29 23:45:30 -0800

    Component update

---
+++ src/components/introComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+                    <h1> Calvin Gao  </h1>
+                    <h3> This is my online Resume </h3>
+                </div>);
+    }
+}
+ 
+export default Intro;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Intro from './components/introComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Intro />
   </React.StrictMode>,
   document.getElementById('root')
 );
