41f2e15f050a4a87c47721cfa2066c03b6c76300
Calvin Gao <c_gao3@yahoo.com>
2020-11-30 21:15:27 -0800

    update portfolio

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,16 @@ const AboutMe = () =>(
     <div>
         <h1>About Me</h1>
         <img src = {calvinPic} alt="Profile" width="150" height= "150"></img>
-        <p> Blah Blah Blah</p>
+        <p>E-mail: calvig1@uci.edu</p>
+        <p>
+
+            Senior studying Computer Science at the University of California, Irvine. I'm passionate about software development and web development. I am actively seeking full time positions/ new grad internships for Software Engineering. I will graduate in March 2021.
+        </p>
+        <p>
+            Specialties:
+            Skills: Java, Python, , C++, SQL, JavaScript, HTML/CSS, Node.js
+            Tool: Eclipse, Visual Studio Code, Git, Unix
+        </p>
     </div>
 )
 
