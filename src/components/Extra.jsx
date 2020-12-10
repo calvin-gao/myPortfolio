bd07f32291c546530ae9bc1c36363c831c83cdcd
Calvin Gao <c_gao3@yahoo.com>
2020-12-10 14:57:09 -0800

    Font update

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -4,8 +4,8 @@ const Extra = () => {
     return (
     <div id="Extra" class="center">
         <h1>Extracurricular</h1>
-        <h2>Riichi Mahjong at UCI  Student President </h2>
-        <h3>Sep. 2019 – 2020 </h3>
+        <h4>Riichi Mahjong at UCI  Student President </h4>
+        <h5>Sep. 2019 – 2020 </h5>
         <ul>
             <li>Succeeded in organizing club events and responsible for club affairs with other organizations.</li>
             <li>Created the organization and motivated many players to improve.</li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,8 @@ const Project = () => {
     return(
         <div id="Projects">
             <h1 class="pad-bot">Projects</h1>
-            <h2>FoodPicker</h2>
-            <h4><em>Implemented with : JavaScript, NodeJS · Sep 2019</em></h4>
-            <h3>Created : Sep 2019</h3>
+            <h4>FoodPicker</h4>
+            <h5><em>Implemented with : JavaScript, NodeJS · Sep 2019</em></h5>
             <p>Link to Project: <a href="https://github.com/calvin-gao/FoodPicker">https://github.com/calvin-gao/FoodPicker</a></p>
             <ul>
                 <li>Created a web app that recommends users restaurants based on user specified parameters.</li>
@@ -14,24 +13,24 @@ const Project = () => {
                 <li>Incorporated YelpAPI and Google Maps Geolocation API.</li>
             </ul>
 
-            <h2>TopMovies</h2>
-            <h4><em>Implemented with : Java, AWS, MySQL  ·  March 2020</em></h4>
+            <h4>TopMovies</h4>
+            <h5><em>Implemented with : Java, AWS, MySQL  ·  March 2020</em></h5>
             <ul>
                 <li>Developed web app selling movies to users using Java Servlets, AWS EC2 Instance, and MySQL.</li>
                 <li>Implemented MySQL queries with JDBC to fetch database information within AWS.</li>
                 <li> Incorporated Connection Pooling and improved query speed by 25%</li>
             </ul>
 
-            <h2>ZotBoba</h2>
-            <h4><em>Implemented with : NodeJS    July 2019</em></h4>
+            <h4>ZotBoba</h4>
+            <h5><em>Implemented with : NodeJS    July 2019</em></h5>
             <ul>
                 <li>Created a RESTful API using the MERN stack, with user authentication.</li>
                 <li>Incorporated responsive web design with fully functional forms.</li>
                 <li>Created personal database for users and posts using MongoDB.</li>
             </ul>
 
-            <h2>W-Warriors</h2>
-            <h4><em>Implemented with : Unity, C#  ·  Jan. 2018 </em></h4>
+            <h4>W-Warriors</h4>
+            <h5><em>Implemented with : Unity, C#  ·  Jan. 2018 </em></h5>
             <ul>
                 <li>Developed a mobile clicker game and launched on the Google Play Store with 500+ users.</li>
                 <li>Assisted in deployment and optimizing run time by 20%.</li>
